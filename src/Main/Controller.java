1767aec4825ae68729eafe7c6442448797f5d273
seohee <zawadi0805@gmail.com>
2021-09-13 13:45:30 +0900

    메인 GUI 생성

--- src/Main/Controller.java
+++ src/Main/Controller.java
@@ -3,10 +3,18 @@ package Main;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class Controller implements Initializable{
+	@FXML
+	
+	ImageView myImageView;
+	Image myImage = new Image(getClass().getResourceAsStream("/img/movie.jpg"));
+	
 	Parent root;
 	public void setRoot(Parent root) {
 		this.root = root;
@@ -15,5 +23,8 @@ public class Controller implements Initializable{
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
 		
+	}
+	public void displayImage() {
+		myImageView.setImage(myImage);
 	}
 }
