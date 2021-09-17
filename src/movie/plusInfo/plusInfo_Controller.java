9dc23c007dc44f103488101a71637130615b437a
dbwls <youjin9874@naver.com>
2021-09-17 11:03:24 +0900

    머징 오류수정

--- src/movie/plusInfo/plusInfo_Controller.java
+++ src/movie/plusInfo/plusInfo_Controller.java
@@ -4,7 +4,6 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
-import Main.mainClass;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -12,10 +11,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.chart.BarChart;
-import javafx.scene.chart.Chart;
-import javafx.scene.chart.PieChart;
-import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
@@ -25,9 +20,6 @@ import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class plusInfo_Controller implements Initializable{
-
-	PieChart pieChart;
-	BarChart barChart;
 	Parent root;
 	ListView<String> fxListView;
 	ImageView fxImageView;
@@ -38,21 +30,11 @@ public class plusInfo_Controller implements Initializable{
 		fxListView = (ListView)root.lookup("#fxListView");
 		fxImageView = (ImageView)root.lookup("#fxImageView");
 		setListView();
-		if(mainClass.s == 2) {
-			pieChart = (PieChart)root.lookup("#pieChart");
-			barChart = (BarChart)root.lookup("#barChart");
-		chart();
-			
 		}
-		mainClass.s += 1;
-	}
 	
 	
 	public void setListView() {
-		if(mainClass.s == 0) {
-			
 			setList();
-			
 			fxListView.getSelectionModel().selectedIndexProperty().addListener((observable, oldValue, newValue)->{
 				System.out.println("observable(형식) : "+observable);
 				System.out.println("oldValue(이전값) : "+oldValue);
@@ -63,7 +45,7 @@ public class plusInfo_Controller implements Initializable{
 				fxImageView.setImage(new Image("/img/movie/"+ url.get( (int)newValue )) );
 			});
 		}
-	}
+	
 	public void setList() {
 		movieTitle = FXCollections.observableArrayList();
 		url = new ArrayList<String>();
@@ -73,68 +55,9 @@ public class plusInfo_Controller implements Initializable{
 	
 		url.add("movie"+ i + ".jpg");
 	}
-		if(mainClass.s == 0) {
 			
 			fxListView.setItems(movieTitle);
 		}
-}
-	
-	public void reserveBtn() {
-			try {
-				Stage primaryStage = new Stage();
-				FXMLLoader loader = 
-						new FXMLLoader(getClass().getResource("MovieInfo.fxml"));
-				Parent Root = loader.load();
-				Scene scene = new Scene(Root);
-				plusInfo_Controller ctl = loader.getController();
-				ctl.setRoot(Root);
-				Stage stage = (Stage)root.getScene().getWindow();
-				stage.close();
-				primaryStage.setScene(scene);
-				primaryStage.show();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}	
-		}
-	public void detailView() {
-		try {
-			Stage primaryStage = new Stage();
-			FXMLLoader loader = 
-					new FXMLLoader(getClass().getResource("detailInfo.fxml"));
-			Parent Root = loader.load();
-			Scene scene = new Scene(Root);
-			plusInfo_Controller ctl = loader.getController();
-			ctl.setRoot(Root);
-			//Stage stage = (Stage)root.getScene().getWindow();
-			//stage.close();
-			primaryStage.setScene(scene);
-			primaryStage.show();
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-	}
-	
-	public void chart() {
-		
-		pieChart.setData(FXCollections.observableArrayList(
-				new PieChart.Data("남성", 54.8),
-				new PieChart.Data("여성", 45.2)
-				));
-
-		XYChart.Series series1 = new XYChart.Series();
-		series1.setName("연령별 예매 분포");
-		series1.setData(FXCollections.observableArrayList(
-					new XYChart.Data("10대",2.30),
-					new XYChart.Data("20대",33.30),
-					new XYChart.Data("30대",33.70),
-					new XYChart.Data("40대",20.30),
-					new XYChart.Data("50대",10.40)
-				));
-		
-		barChart.getData().add(series1);
-	}
 	
 		
 
