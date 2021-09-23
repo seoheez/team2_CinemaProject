0e194af6cddb5f7b470ed2c8b386bc97a404a15b
seohee <zawadi0805@gmail.com>
2021-09-23 14:54:03 +0900

    Chart 수치 변경

--- src/movie/plusInfo/datail_Info_Controller0.java
+++ src/movie/plusInfo/datail_Info_Controller0.java
@@ -18,25 +18,59 @@ public class datail_Info_Controller0 implements Initializable{
 	Parent root;
 	PieChart pieChart;
 	BarChart barChart;
-
-	public void setRoot(Parent root,String s) {
+	
+	public void setRoot(Parent root,String[] s) {
 		this.root = root;
 		pieChart = (PieChart)root.lookup("#pieChart");
 		barChart = (BarChart)root.lookup("#barChart");
-		chart();
+		chart(s[2]);
 		Label a = (Label)root.lookup("#story");
 		Label b = (Label)root.lookup("#outline");
-		a.setText(s);
-		b.setText(s);
+		a.setText(s[0]);
+		b.setText(s[1]);
 	}
 	
-public void chart() {
+public void chart(String ss) {
+		double[] gender0 = { 53.7, 46.3};//샹치
+		double[] gender1 = { 44.3, 55.7};//인질
+		double[] gender2 = { 39.1, 60.9};//포켓몬스터
+		double[] gender3 = { 42.6, 57.4};//모가디슈
+		double[] gender4 = { 51.8, 48.2};//건파우더
+		double[] gender5 = { 44.8, 55.2};//맨인더다크2
 		
-		pieChart.setData(FXCollections.observableArrayList(
-				new PieChart.Data("남성", 70.8),
-				new PieChart.Data("여성", 29.2)
-				));
-
+		//반복문 생각하기
+		if(ss.equals("0"))
+		{pieChart.setData(FXCollections.observableArrayList(
+				new PieChart.Data("남성", gender0[0]),
+				new PieChart.Data("여성", gender0[1])
+				));}
+		if(ss.equals("1"))
+		{pieChart.setData(FXCollections.observableArrayList(
+				new PieChart.Data("남성", gender1[0]),
+				new PieChart.Data("여성", gender1[1])
+				));}
+		if(ss.equals("2"))
+		{pieChart.setData(FXCollections.observableArrayList(
+				new PieChart.Data("남성", gender2[0]),
+				new PieChart.Data("여성", gender2[1])
+				));}
+		if(ss.equals("3"))
+		{pieChart.setData(FXCollections.observableArrayList(
+				new PieChart.Data("남성", gender3[0]),
+				new PieChart.Data("여성", gender3[1])
+				));}
+		if(ss.equals("4"))
+		{pieChart.setData(FXCollections.observableArrayList(
+				new PieChart.Data("남성", gender4[0]),
+				new PieChart.Data("여성", gender4[1])
+				));}
+		if(ss.equals("5"))
+		{pieChart.setData(FXCollections.observableArrayList(
+				new PieChart.Data("남성", gender5[0]),
+				new PieChart.Data("여성", gender5[1])
+				));}
+		
+		
 		XYChart.Series series1 = new XYChart.Series();
 		series1.setName("연령별 예매 분포");
 		series1.setData(FXCollections.observableArrayList(
