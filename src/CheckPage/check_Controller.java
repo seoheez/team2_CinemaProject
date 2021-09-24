613800abd4062172794a4a8ffd5e5c2446f2f7a4
hyeonwoo <ejffoddl1@naver.com>
2021-09-24 13:36:05 +0900

    d

--- src/CheckPage/check_Controller.java
+++ src/CheckPage/check_Controller.java
@@ -5,14 +5,26 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.control.Label;
 import sign.loginService.LoginService;
 import sign.member.MemberProc;
 
 public class check_Controller implements Initializable{
 	Parent root;
-
-	public void setRoot(Parent root) {
+	Label fxTitle, fxTime, fxPCount, fxSNum, fxDate;
+	public void setRoot(Parent root,String title, String time, String count, String date,String num) {
 		this.root = root;
+		fxTitle = (Label)root.lookup("#fxTitle");
+		fxTime = (Label)root.lookup("#fxTime");
+		fxPCount = (Label)root.lookup("#fxPCount");
+		fxSNum	 = (Label)root.lookup("#fxSNum");
+		fxDate = (Label)root.lookup("#fxDate");
+		fxTitle.setText(title);
+		fxTime.setText(time);
+		fxPCount.setText(count);
+		fxSNum.setText(date);
+		fxDate.setText(num);
+		
 		
 	}
 	@Override
